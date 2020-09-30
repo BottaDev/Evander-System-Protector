fcded79a60d1b3961997143612ebf8957259d54f
BottaDev <fedebotta@gmail.com>
2020-09-29 23:15:38 -0300

    Added test main menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,16 +7,9 @@ public class LevelManager : MonoBehaviour
 {
     UIManager manager;
 
-    private int currentLevelIndex;
-
     private void Awake()
     {
         manager = GetComponent<UIManager>();
-    }
-
-    private void Start()
-    {
-        currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     public void WinLoseGame(GameObject entity)
@@ -25,10 +18,5 @@ public class LevelManager : MonoBehaviour
             manager.ShowFinalGui(true);
         else if (entity.layer == 8)
             manager.ShowFinalGui(false);
-    }
-
-    public void RetryLevel()
-    {
-        SceneManager.LoadScene(currentLevelIndex);
     }
 }
